df69c0651f0026572574fd1c671f1c7491308b3f
byungchan0725 <byungchan0725@gmail.com>
2024-10-06 14:25:24 +0900

    Refactor :: 서킷 정보 분리

---
+++ src/data/circuitData.js
@@ -0,0 +1,18 @@
+// src/data/circuitData.js
+export const circuitData = [
+  {
+    id: 1,
+    name: '서킷 이름 1',
+    country: 'KR',
+    circuit: '서킷 정보 1',
+  },
+  {
+    id: 2,
+    name: '서킷 이름 2',
+    country: '국가 2',
+    circuit: '서킷 정보 2',
+  },
+  // ... 추가 서킷 데이터
+];
+
+export default circuitData;
