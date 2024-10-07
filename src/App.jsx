09451ef5a2e7c60ef6524ac5683508ab9b94dcb9
byungchan0725 <byungchan0725@gmail.com>
2024-10-07 22:32:57 +0900

    Style :: 눈 터질거같아서 검은색으로 바꿈 & Feat :: 질문 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './ui/Header/Header';
-import Garage from './asset/svg/garage.svg';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log(searchText);
+      setSearchText('');
+    }
+  };
+
   return (
-    <Router>
-      <div>
-        <header className="flex items-center justify-center bg-red-600 h-[60px]">
-          <h1 className="text-white text-xl font-bold">F1 Insight</h1>
-        </header>
-        <div className="w-full max-w-[60%] mx-auto mt-10">
-          <h1 className="text-2xl font-nanum mb-3">F1 지식 습득 플랫폼</h1>
-          <p className="text-sm font-light font-nanum mb-5">
-            ~~~~~~~~~~~~~~~~~~
-          </p>
-          <p className="text-sm font-light font-nanum mb-5">
-            디자인 해야함 디자인 해야함 디자인 해야함 디자인 해야함
-          </p>
-          <div className="flex flex-col items-center justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 lg:w-[80%]">
-              <div className="border-2 border-gray-200 rounded-lg h-[200px] flex flex-col items-center overflow-hidden shadow-md hover:shadow-red-300">
-                <img
-                  src={Garage}
-                  alt="garage"
-                  className="w-[60px] h-auto mt-8 md:w-[20%]"
-                />
-                <h1 className="mt-2 text-2xl font-nanum">팀 확인</h1>
-                <p className="text-center font-light font-nanum mt-4 px-4 flex-grow">
-                  {new Date().getFullYear()} 시즌 팀을 확인합니다.
-                </p>
-              </div>
+    <div className='h-screen'>
+      <div className='w-full flex items-center justify-center h-[50px] bg-neutral-900'>
+        <p className='text-white text-xl font-bold'>F1 Insight</p>
+      </div>
+      <div className='flex justify-center h-full'>
+        <div className='w-full bg-neutral-800 flex justify-center'>
+          <div className='w-[70%] h-full bg-neutral-800'>
+            <div className="mt-8 flex justify-center">
+              <input
+                type="text"
+                placeholder="검색어를 입력하세요"
+                className="w-2/3 px-4 py-2 text-white rounded-l-lg border border-gray-500 focus:outline-none bg-neutral-800"
+                value={searchText}
+                onChange={handleInputChange}
+                onKeyPress={handleKeyPress}
+              />
+              <button className="bg-neutral-700 hover:bg-neutral-600 text-white px-4 py-2 rounded-r-lg focus:outline-none transition duration-300">
+                검색
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </Router>
+    </div>
   );
 };
 
